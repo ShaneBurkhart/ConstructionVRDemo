da354d06c5d99b59c34b80b319b155fd9a521bd7
Shane Burkhart <shaneburkhart@gmail.com>
2019-04-08 13:11:19 -0500

    Add magic notification for renderings.

--- workers/skp_rendering_notification.rb
+++ workers/skp_rendering_notification.rb
@@ -23,9 +23,23 @@ def create_unit_version_rendering_completed_notification_message(unit_version)
   return "Done rendering #{unit_name} unit in #{project_name}. <#{admin_link}|View> - <#{unit_feedback_link}|Feedback>"
 end
 
-def send_update_hotspots_email_to_magic_team(unit_version)
-  # TODO Send email to magic team email.
-  # Start with sending it to Meghan in slack.
+def create_update_hotspots_notification_to_magic_team(unit_version)
+  unit = unit_version.unit
+  project = unit.project
+
+  unit_name = unit["Name"]
+  project_name = project["Name"]
+  project_prod_link = project["Prod Link"]
+  project_prod_admin_link = project["Prod Admin Login Link"]
+
+  uri = URI::parse(project_prod_link)
+  project_prod_path = uri.path
+
+  unit_path = "#{project_prod_path}/unit/#{unit.id}?show_tour=1&version=#{URI.encode_www_form_component(unit_version.id)}"
+  admin_link = "#{project_prod_admin_link}?redirect_to=#{URI.encode_www_form_component(unit_path)}"
+  unit_feedback_link = "#{project_prod_link}/unit/#{unit.id}/feedback_feed"
+
+  return "Hotspots need updated for unit #{unit_name} in #{project_name}. <#{admin_link}|Update Hotspots> <#{}|How To>"
 end
 
 loop do
@@ -35,7 +49,8 @@ loop do
     slack_message = create_unit_version_rendering_completed_notification_message(unit_version)
     send_slack_message_to_rendering_channel(slack_message)
 
-    send_update_hotspots_email_to_magic_team(unit_version)
+    magic_message = create_update_hotspots_notification_to_magic_team(unit_version)
+    send_slack_message_to_magic_channel(magic_message)
 
     unit_version["Notified At"] = Time.now
     unit_version.save
