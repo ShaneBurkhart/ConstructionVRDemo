eea75362379f55eaf7cfd49dbdcedc017a118a55
Shane Burkhart <shaneburkhart@gmail.com>
2020-03-04 12:53:45 -0600

    Remove region.

--- server.rb
+++ server.rb
@@ -99,7 +99,6 @@ post '/api/temp_upload/presign' do
   key = "tmp/#{SecureRandom.uuid}_#{params['filename']}"
   signer = Aws::S3::Presigner.new
   url = signer.presigned_url(:put_object, {
-    region: ENV["REGION"],
     bucket: ENV["BUCKET"],
     key: key,
     content_type: params["mime"],
