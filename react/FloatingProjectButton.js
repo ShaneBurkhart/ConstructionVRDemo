422c4cffd8aa894b28a3700d6eecca24570bda3a
Shane Burkhart <shaneburkhart@gmail.com>
2020-04-15 17:20:50 -0500

    hide floating button on print.

--- react/FloatingProjectButton.js
+++ react/FloatingProjectButton.js
@@ -12,7 +12,7 @@ class FloatingProjectButton extends React.Component {
     const { name } = this.props;
 
     return (
-      <div className="floating-project-button-container">
+      <div className="floating-project-button-container hide-print">
         <Button icon labelPosition="right" onClick={_=>window.scrollTo(0,0)}>
           {name}
           <Icon name="up arrow" />
