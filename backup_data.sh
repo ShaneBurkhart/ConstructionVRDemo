0538f742b9e0a582f7b6d7f626a0585ee25cbd93
Ubuntu <ubuntu@ip-172-31-24-191.us-west-2.compute.internal>
2020-06-23 01:32:25 +0000

    backup script.

--- backup_data.sh
+++ backup_data.sh
@@ -1,4 +1,4 @@
 ZIP_NAME=`date +"%Y-%m-%d"_backup.zip`
-zip -r /home/ubuntu/${ZIP_NAME} /data/finish-vision* /etc/letsencrypt
+sudo zip -r /home/ubuntu/${ZIP_NAME} /home/ubuntu/ConstructionVRDemo-Prod/data /etc/letsencrypt
 docker run --rm --env-file /home/ubuntu/ConstructionVRDemo-Prod/user.env -v /home/ubuntu:/data amazon/aws-cli s3 cp /data/${ZIP_NAME} s3://finish-vision-vr/backups/${ZIP_NAME}
-rm /home/ubuntu/${ZIP_NAME}
+rm -f /home/ubuntu/${ZIP_NAME}
