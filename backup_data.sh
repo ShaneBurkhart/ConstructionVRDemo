19e1e8b96759bb153bc1ab50035115d06352759b
Shane Burkhart <shaneburkhart@gmail.com>
2020-06-22 20:18:39 -0500

    Wrong directory.

--- backup_data.sh
+++ backup_data.sh
@@ -1,4 +1,4 @@
 ZIP_NAME=`date +"%Y-%m-%d"_backup.zip`
-zip -r /home/ubuntu/${ZIP_NAME} /data /etc/letsencrypt
+zip -r /home/ubuntu/${ZIP_NAME} /data/finish-vision* /etc/letsencrypt
 docker run --rm --env-file /home/ubuntu/ConstructionVRDemo-Prod/user.env -v /home/ubuntu:/data amazon/aws-cli s3 cp /data/${ZIP_NAME} s3://finish-vision-vr/backups/${ZIP_NAME}
 rm /home/ubuntu/${ZIP_NAME}
