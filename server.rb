2e3caff0fd81b337e4cd09cab141c79c2e99b152
Kevin Boyle <boyle.kevin.michael@gmail.com>
2021-02-26 10:32:55 -0800

    fix function creating new Feedback message for screenshot feedback, fix slack channel message function

--- server.rb
+++ server.rb
@@ -117,7 +117,7 @@ post '/api/temp_upload/presign' do
   content_type "application/json"
   {
     presignedURL: url,
-    awsURL: "https://finish-vision-vr.s3-us-west-2.amazonaws.com/#{key}"
+    awsURL: "https://#{ENV["BUCKET"]}.s3-us-west-2.amazonaws.com/#{key}"
   }.to_json
 end
 
@@ -688,20 +688,19 @@ post '/project/:access_token/screenshot/feedback' do
   return "Not found" if unit_version.nil? or !unit_version.unit.belongs_to_project?(project)
   return "Not found" if unit_version["Pano Versions"].nil? or unit_version["Pano Versions"].length < 1
 
-  feedback = Feedback.new(
+  feedback = Feedback.create(
     "Pano Version" => [unit_version["Pano Versions"].first],
     "Notes" => notes,
     "Is Fix" => true,
     "Screenshot" => [{ url: image_url }],
   )
 
-  feedback.create
 
   fields = feedback.fields
   fields["Notes HTML"] = feedback.notes_html
 
   # Send notification to slack
-  send_slack_message_to_rendering_channel(create_feedback_notification_message(feedback))
+  send_slack_message_to_rendering_channel(create_feedback_notification_message(feedback, unit_version))
 
   content_type :json
   return fields.to_json
@@ -729,6 +728,7 @@ post '/project/:access_token/pano/:id/feedback' do
 
   feedback = Feedback.new(
     "Pano Version" => [pano_version.id],
+    "Unit Version Id" => unit_version_id,
     "Notes" => params["notes"],
     "View Parameters" => params["viewParameters"],
     "Is Fix" => is_fix == true ? true : nil
