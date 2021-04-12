f54c50e2b0aa657b3631d9c3723eafe146574ba5
Kevin Boyle <boyle.kevin.michael@gmail.com>
2021-04-12 10:13:50 -0700

    add ability to rename site via focuseditableinput @ site name (h1)

--- react/finishes_revision/SiteHeader.js
+++ react/finishes_revision/SiteHeader.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import ActionCreators from './action_creators';
 
 import FocusEditableInput from '../components/FocusEditableInput';
 
 const SiteHeader = ({ projectName }) => {
   const renderingsLink = window.hasOwnProperty("RENDERINGS_LINK") ? RENDERINGS_LINK : "";
+  const projectId = useSelector(state => state.projectId)
+  const adminMode = IS_SUPER_ADMIN || IS_EDITOR;
+  
+  const changeProjectName = (newName) => {
+    if (newName && newName !== projectName) ActionCreators.changeProjectName(projectId, newName);
+  }
+  
   return (
     <div className="xlarge-container">
-      <h1>{projectName}</h1>
+      <h1 style={{ display: "inline-block" }}>
+        <FocusEditableInput
+          editable={adminMode}
+          value={projectName}
+          onUpdate={changeProjectName}
+        />
+      </h1>
       <div className="ui tabular menu hide-print">
         <a className="item active">Finish Selections</a>
         {renderingsLink && (
