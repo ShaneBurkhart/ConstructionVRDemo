0ae0b98543aee9c83f5672402f02b166df3ddad4
Kevin Boyle <boyle.kevin.michael@gmail.com>
2021-08-25 19:53:02 +0000

    redo script w/ current data structure associations

--- packages/web/scripts/migrate_projectDocUrl_to_plan_document.js
+++ packages/web/scripts/migrate_projectDocUrl_to_plan_document.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('postgres://postgres:postgres@pg:5432/mydb')
 var models = require("../models/index.js");
-const queue = require("lambda-queue"); //TODO: how to import this?
 
 async function wait(ms) {
   return new Promise(resolve => {
@@ -17,25 +16,22 @@ async function wait(ms) {
 
   for (const project of projects) {
     const { documentUrl } = project;
+    
     if (documentUrl) {
+      const document = await models.Document.create({
+        s3Url: documentUrl,
+        filename: documentUrl, //TODO: can I get the filename?
+        //TODO: filetype (?)
+        startedPipelineAt: Date.now(),
+      });
+      
       const order = (project.Plans || []).length;
-      const plan = await project.createPlan({
+      await project.createPlan({
+        DocumentId: document.id,
         name: documentUrl,
         order,
         uploadedAt: Date.now(),
       });
-      
-      const document = await plan.createDocument({
-        s3Url: documentUrl,
-        filename: documentUrl, //TODO: can I get the filename?
-        startedPipelineAt: Date.now(),
-      });
-      
-      queue.startSplitPdf({
-        's3Key': encodeURIComponent(document.s3Url),
-        'objectId': document.uuid
-      });
-
     };
   }
 
