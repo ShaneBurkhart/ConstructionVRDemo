3750d4c6c0e5f5b9b227173a9675292b0ca3ecd5
Kevin Boyle <boyle.kevin.michael@gmail.com>
2021-08-27 03:42:58 +0000

    bring in snappyPDF components for document viewer

---
+++ packages/web/react/document_viewer/pages/Document.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const Document = (props) => {
+	const { documentUuid } = useParams()
+	const [loading, setLoading] = useState(true)
+	const [progress, setProgress] = useState(null)
+	const [document, setDocument] = useState(null)
+
+	const sheets = (document || {}).Sheets || []
+	sheets.sort((a, b) => a.index < b.index ? -1 : (a.index > b.index ? 1 : 0))
+
+	// const getDocumentRequest = bouncer.get("/api/documents/" + documentUuid)
+
+	// console.log(loading, progress, document)
+
+	const checkDocument = () => {
+		// getDocumentRequest.sendRequest(null, doc => {
+		// 	setDocument(doc)	
+
+		// 	if (!doc.pageCount || (doc.Sheets || []).length !== doc.pageCount) {
+		// 		setTimeout(checkDocument, 1000)
+		// 	} else {
+		// 		setLoading(false)
+		// 	}
+		// }, err => {
+		// 	alert(err)
+		// })
+	}
+
+	useEffect(() => { checkDocument() }, [])
+
+	return (
+		<>
+			Document {documentUuid}
+			{sheets.map(sheet => {
+				return (
+					<img key={sheet.index} src={sheet.fullImgUrl} />
+				)
+			})}
+		</>
+	)
+}
+
+export default Document
